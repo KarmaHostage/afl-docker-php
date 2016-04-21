b455eb86d3f8380979a3427a5956374c93ed7f49
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-21 14:22:47 +0200

    adding example files to fuzz

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,29 @@
 #!/bin/sh
-touch .initialized
+
+#will be creating our inputdirectory and synchronisation directory
+INPUTDIR="/fuzzing/inputs"
+SYNCDIR="/fuzzing/syncdir"
+
+mkdir -p "$INPUTDIR"
+mkdir -p "$SYNCDIR"
+
+# init
+# look for empty dir
+if [ "$(ls -A $INPUTDIR)" ]; then
+     echo "No need to add an example php file, $DIR is not empty"
+else
+    echo "/examples directory contains:"
+    ls -l /examples
+    echo "$INPUTDIR is Empty, going to copy our examples"
+    cp /examples/* /fuzzing/inputs/.
+    echo "/fuzzing/inputs directory now contains files:"
+    ls -l /fuzzing/inputs
+fi
+
+if [ "$(ls -A $SYNCDIR)" ]; then
+     echo "$SYNCDIR is not empty, going to resume"
+     nohup afl-fuzz -m 200 -i - -o /fuzzing/syncdir -M php-master-fuzzer php @@  > eventlog.log &
+else
+    echo "$SYNCDIR is empty, going to start new"
+    nohup afl-fuzz -m 200 -i /fuzzing/inputs -o /fuzzing/syncdir -M php-master-fuzzer php @@  > eventlog.log &
+fi
